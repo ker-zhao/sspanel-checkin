e435a47892bc653848498e958f8a83ab4d603a6d
KolenZhao <874445070@qq.com>
2021-08-05 18:58:57 +0800

    Create sspanel_checkin.py

---
+++ sspanel_checkin.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+
+def push_message(send_key, title, body):
+    url = 'https://sc.ftqq.com/{}.send'.format(send_key)
+    data = {
+        'text': title,
+        'desp': body,
+    }
+    requests.post(url=url, data=data)
+
+
+def main():
+    user_info, send_key = os.environ['USER_INFO'], os.environ['SEND_KEY']
+    base_url, email, password = user_info.split("|")
+    login_url = base_url + '/auth/login'
+    checkin_url = base_url + '/user/checkin'
+
+    with requests.sessions.Session() as session:
+        post_data = {"email": email, "passwd": password, "code": ""}
+        session.post(login_url, post_data)
+        res = session.post(checkin_url)
+    # message = str(res.json())
+    message = res.content.decode('unicode_escape')
+    print(message)
+    push_message(send_key, "SSPanel报名结果", message)
+
+
+if __name__ == "__main__":
+    main()
